4d2b2e8b0c60ddafa639c9bb42ba0e55dfb4851b
BOB KELLY OVIE WILLIAMS <bobkellywilliams@gmail.com>
2022-11-15 15:25:49 +0100

    javascript edited again

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let seconds = 00; 
 let tens = 00; 
+let minutes = 00
 let appendTens = document.getElementById("tens")
 let appendSeconds = document.getElementById("seconds")
+let appendMinutes = document.getElementById("minutes")
 let buttonStart = document.getElementById('button-start');
 let buttonStop = document.getElementById('button-stop');
 let buttonReset = document.getElementById('button-reset');
@@ -22,13 +24,15 @@ buttonStart.onclick = function() {
     clearInterval(Interval);
     tens = "00";
   	seconds = "00";
+    minutes = "00"
     appendTens.innerText = tens;
   	appendSeconds.innerText = seconds;
-}
-
-function startTimer () {
+    appendMinutes.innerText = minutes;
+  }
+  
+  function startTimer () {
     tens++; 
-    
+       
     if(tens <= 9){
       appendTens.innerText = "0" + tens;
     }
@@ -49,5 +53,26 @@ function startTimer () {
     if (seconds > 9){
       appendSeconds.innerText = seconds;
     }
+
+    if (seconds > 59) {
+      minutes++;
+      appendMinutes.innerText = "0" + minutes;
+      seconds = 0;
+      appendSeconds.innerText = "00";
+      console.log(appendSeconds);
+    }
+
+    // if (minutes = seconds >= 59) {
+    //   appendMinutes.innerText = minutes
+    //   minutes = 0;
+    // }
+
+    // if (seconds > 59) {
+    //   console.log("minutes");
+    //   // seconds = 0
+    //   minutes++;
+    //   appendMinutes.innerText = `${minutes} ${seconds} ${tens} + 0`
+    //   appendMinutes.innerText = "1" + 0;
+    // } 
 }
   
